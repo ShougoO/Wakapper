64741193fb7710d20d2fa7201d916654edf35bd9
ShougoO <d31408so@apps.kct.ac.jp>
2017-10-22 21:09:42 +0900

    GoogleMap-azureデバッグ用

--- Wakapper/src/pages/page1/page1.ts
+++ Wakapper/src/pages/page1/page1.ts
@@ -18,6 +18,8 @@ import {
 export class Page1 {
   map: GoogleMap;
   mapElement: HTMLElement;
+  lat: number;
+  lng: number;
 
   text: string;
   showText: string;
@@ -26,20 +28,11 @@ export class Page1 {
   items: Array<{title: string, note: string, icon: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private googleMaps: GoogleMaps) {
-    this.mapElement = document.getElementById('map_canvas');
-    let mapOptions: GoogleMapOptions = {
-      camera: {
-        target: {
-          lat: 43.0741904,
-          lng: -89.3809802
-        },
-        zoom: 18,
-        tilt: 30
-      }
-    };
-    this.map = this.googleMaps.create(this.mapElement, mapOptions);
-    
     this.text = navParams.get("text");
+    this.lat = navParams.get("lat");
+    this.lng = navParams.get("lng");
+    alert(this.lat);
+    alert(this.lng);
     this.showText = this.text;
     
     // If we navigated to this page, we will have an item available as a nav param
@@ -57,6 +50,46 @@ export class Page1 {
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
     }
+  }
+
+  ionViewDidLoad(){
+    this.loadMap();
+  }
+
+  loadMap() {
+    this.mapElement = document.getElementById('map_canvas');
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: this.lat,
+          lng: this.lng
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    };
+    this.map = this.googleMaps.create(this.mapElement, mapOptions);
+    alert('created');
+
+    // Wait the MAP_READY before using any methods.
+    this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
+      alert('Map is ready!');
+
+      // Now you can use all methods safely.
+      this.map.addMarker({
+        title: 'Ionic',
+        icon: 'blue',
+        animation: 'DROP',
+        position: {
+          lat: 43.0741904,
+          lng: -89.3809802
+        }
+      }).then(marker => {
+        marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+          alert('clicked');
+        });
+      });
+    });
   }
 
   itemTapped(event, item) {
