8e37f3dbe98bb528e295c1b052e6e2c45d0ba61d
Kentarou Ueda <ueken0307@hotmail.co.jp>
2017-10-28 12:24:33 +0900

    [update]page4の更新

--- Wakapper/src/pages/page4/page4.ts
+++ Wakapper/src/pages/page4/page4.ts
@@ -6,38 +6,84 @@ import { NavController, NavParams } from 'ionic-angular';
   selector: 'page-page4',
   templateUrl: 'page4.html'
 })
+
 export class Page4 {
-  text: string;
-  showText: string;
   selectedItem: any;
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
 
+  //表示する文字列
+  title: string[];
+  note: string[];
+
+  //交通手段
+  traffic: string[];
+  //駅
+  train: string[];
+  //バス停
+  bus: string[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.text = navParams.get("text");
-    this.showText = this.text;
 
     // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
+    this.selectedItem = navParams.get('title');
+    if (this.selectedItem == undefined) {
+      this.selectedItem = '運行表'
+    }
+
+    this.traffic = ['渡船','電車','バス'];
+    this.train = ['若松駅', '奥洞海駅', '藤ノ木駅', '二島駅'];
+    this.bus = ['バス停1', 'バス停2', 'バス停3', 'バス停4'];
+
+    
 
     // Let's populate this page with some filler content for funzies
     this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
     'american-football', 'boat', 'bluetooth', 'build'];
 
+    
+    this.title = this.traffic;
+    if (this.selectedItem == this.traffic[0]) {
+      //時刻表ファイルを読み込む
+      //テスト用のやつ
+      this.title = [`上り`, ` 8:30`, ` 9:45`, ` 10:30`, ` 11:30`, `下り`, ` 8:30`, ` 9:45`, ` 10:30`, ` 11:30`];
+    }
+    if (this.selectedItem == this.traffic[1]) {
+      this.title = this.train;
+    }
+    if (this.selectedItem == this.traffic[2]) {
+      this.title = this.bus;
+    }
+
+    for (let i = 0; i < this.train.length; i++) {
+      if (this.selectedItem == this.train[i]) {
+        //時刻表ファイルを読み込む
+        //テスト用のやつ
+        this.title = [`上り`, ` 8:30`, ` 9:45`, ` 10:30`, ` 11:30`, `下り`, ` 8:30`, ` 9:45`, ` 10:30`, ` 11:30`];
+      }
+    }
+
+    for (let i = 0; i < this.bus.length; i++) {
+      if (this.selectedItem == this.bus[i]) {
+        //時刻表ファイルを読み込む
+        //テスト用のやつ
+        this.title = [`上り`, ` 8:30`, ` 9:45`, ` 10:30`, ` 11:30`, `下り`, ` 8:30`, ` 9:45`, ` 10:30`, ` 11:30`];
+      }
+    }
+
+
     this.items = [];
-    for (let i = 1; i < 11; i++) {
+    for (let i = 0; i < this.title.length; i++) {
       this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+        title: this.title[i],
+        note: '',
+        icon: '',
       });
     }
   }
 
   itemTapped(event, item) {
     // That's right, we're pushing to ourselves!
-    this.navCtrl.push(Page4, {
-      item: item
-    });
+    this.navCtrl.push(Page4, {title: item});
   }
 }
