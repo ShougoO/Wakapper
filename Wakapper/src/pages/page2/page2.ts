ae47838ff8a385996824db66c11966667a2ac921
ShougoO <d31408so@apps.kct.ac.jp>
2017-10-28 12:59:09 +0900

    マップ表示

--- Wakapper/src/pages/page2/page2.ts
+++ Wakapper/src/pages/page2/page2.ts
@@ -19,7 +19,7 @@ export class Page2 {
    
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
-
+    alert(this.selectedItem);
     // Let's populate this page with some filler content for funzies
     this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
     'american-football', 'boat', 'bluetooth', 'build'];
